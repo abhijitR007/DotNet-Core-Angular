8f9eeb12696cd090c78eea0a4b8b70572313071b
abhijitR007 <abhijitrokade007@gmail.com>
2020-01-26 21:07:05 +0530

    Sample API

--- DatingApp/DatingApp.API/Controllers/WeatherForecastController.cs
+++ DatingApp/DatingApp.API/Controllers/WeatherForecastController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace DatingApp.API.Controllers
@@ -11,6 +14,8 @@ namespace DatingApp.API.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        public DataContext _context { get; }
+    
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -18,9 +23,10 @@ namespace DatingApp.API.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger,DataContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         [HttpGet]
@@ -34,6 +40,31 @@ namespace DatingApp.API.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpGet("allnames")]
+        public async Task<IActionResult> GetAllNames()
+        {
+            var values= await _context.Values.ToListAsync();
+            return Ok(values);
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetName( int id)
+        {
+            var values=_context.Values.FirstOrDefault(x=>x.id==id);
+            return Ok(values);
+
+        }
+        
+        [HttpPost("addname")]
+        public async Task<IActionResult> AddNames(Value value12)
+        {
+         await _context.Values.AddAsync(value12);
+           await _context.SaveChangesAsync();
+            return Ok(value12);
+
         }
     }
 }
